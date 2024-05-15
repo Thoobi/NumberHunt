c4dce30d52a1f377aa5319de2a46855da199f9f7
Daniel <105226307+Thoobi@users.noreply.github.com>
2024-05-15 22:58:59 +0100

    added feature

--- game.js
+++ game.js
@@ -188,3 +188,14 @@ const exitGame = () =>
     }
 }
 exitGame()
+
+var elts = document.getElementsByClassName('test')
+Array.from(elts).forEach(function(elt){
+  elt.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13 || elt.value.length == 1) {
+      // Focus on the next sibling
+      elt.nextElementSibling.focus()
+    }
+  });
+});
